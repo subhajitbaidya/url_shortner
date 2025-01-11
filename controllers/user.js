56ec9bedf7983d10254301c957201c07c6912df9
Subhajit Baidya <subhajitbaidy43@gmail.com>
2025-01-12 03:24:51 +0530

    Adding statefull auhentication

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const { v4: uuidv4 } = require("uuid");
+const { setUser } = require("../service/auth");
 
 async function handleUserSignup(req, res) {
   const { name, email, password } = req.body;
@@ -17,6 +19,10 @@ async function handleUserLogin(req, res) {
     return res.render("login", {
       error: "Invalid Username or Password",
     });
+
+  const sessionId = uuidv4();
+  setUser(sessionId, user);
+  res.cookie("uid", sessionId);
   return res.redirect("/");
 }
 
