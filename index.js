6fda997a0ad2e787beea4c1ef6de2f400f12d270
Subhajit Baidya <subhajitbaidy43@gmail.com>
2025-01-19 00:56:49 +0530

    minor changes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.set("views", path.resolve("./views"));
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser());
 
 app.use("/url", restrictToLoggedinUsersOnly, urlRoute);
